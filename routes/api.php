44764fdb0fb500c1c0a1969020c25c1e328faa15
RRoland <roli.rozsalyi@gmail.com>
2024-11-04 12:20:36 +0100

    patch->put

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('/user/login', [UserController::class, 'login']);
 Route::post('/user/register', [UserController::class,'register']);
 // Route::patch("user/update/admin/{id}", [UserController::class,"updateAdmin"])->middleware(["auth:sanctum", "abilities:user-update-admin"]);
 // Route::patch("user/update/everyone/{id}", [UserController::class,"updateEveryone"])->middleware(["auth:sanctum", "abilities:user-update-everyone"]);
-Route::patch("user/update/{id}", [UserController::class,"update"])->middleware(["auth:sanctum", "abilities:user-update"]);
+Route::put("user/update/{id}", [UserController::class,"update"])->middleware(["auth:sanctum", "abilities:user-update"]);
 Route::patch("user/update/privilege/{id}", [UserController::class,"makeUserAdmin"])->middleware(["auth:sanctum", "abilities:user-update-admin"]);
 Route::delete("/user/{id}", [UserController::class, "delete"])->middleware(["auth:sanctum", "abilities:user-delete"]);
 Route::delete("/user/restore/{id}", [UserController::class, "restore"])->middleware(["auth:sanctum", "abilities:user-delete"]);
