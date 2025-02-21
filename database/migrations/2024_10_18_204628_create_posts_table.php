f7b90407e35e287d2e1d5d47e1086cd32daeaef8
Secret198 <coconutisthebest3@gmail.com>
2025-02-21 11:57:43 +0100

    removed comments

--- database/migrations/2024_10_18_204628_create_posts_table.php
+++ database/migrations/2024_10_18_204628_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('post');
-            $table->string('image')->nullable();        //Ez még kérdéses
+            $table->string('image')->nullable();        
             $table->integer('likes');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,6 @@ Route::post('/user/login', [UserController::class, 'login']);
 Route::post('/user/register', [UserController::class,'register']);
 Route::get("/user/leader/{leaderByStr}", [UserController::class, "leaderboard"]);
 Route::patch('/user/logout', [UserController::class,'logout'])->middleware(["auth:sanctum", "abilities:user-view"]);
-// Route::patch("user/update/admin/{id}", [UserController::class,"updateAdmin"])->middleware(["auth:sanctum", "abilities:user-update-admin"]);
-// Route::patch("user/update/everyone/{id}", [UserController::class,"updateEveryone"])->middleware(["auth:sanctum", "abilities:user-update-everyone"]);
 Route::put("user/update/{id}", [UserController::class,"update"])->middleware(["auth:sanctum", "abilities:user-update"]);
 Route::patch("user/update/privilege/{id}", [UserController::class,"makeUserAdmin"])->middleware(["auth:sanctum", "abilities:user-update-admin"]);
 Route::delete("/user/{id}", [UserController::class, "delete"])->middleware(["auth:sanctum", "abilities:user-delete"]);
